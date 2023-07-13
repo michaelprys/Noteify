d7aa6371d0500c29c3b12547a6a916c45af09810
michaelprys <musha09cz@gmail.com>
2023-07-13 16:31:08 +0200

    first init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import App from "./App.vue";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [],
+});
+createApp(App).use(router).mount("#app");
