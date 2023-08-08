8c8ad367530bd36f7ae145ac323f5ea3d6be4dcc
michaelprys <musha09cz@gmail.com>
2023-08-09 01:52:07 +0200

    modal close button functionality

--- src/components/Notes/ModalDeleteNote.vue
+++ src/components/Notes/ModalDeleteNote.vue
@@ -4,15 +4,39 @@
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">Please, confirm.</p>
-        <button class="delete" aria-label="close"></button>
+        <button @click="closeModal" class="delete" aria-label="close"></button>
       </header>
       <section class="modal-card-body">
         Are you sure you want to delete this note?
       </section>
       <footer class="modal-card-foot is-justify-content-right">
         <button class="button is-danger">Delete</button>
-        <button class="button">Cancel</button>
+        <button @click="closeModal" class="button">Cancel</button>
       </footer>
     </div>
   </div>
 </template>
+
+<script setup>
+// imports
+import { onClickOutside } from "@vueuse/core";
+
+// props
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// emits
+
+const emit = defineEmits(["update:modelValue"]);
+
+// close modal
+
+const closeModal = () => {
+  emit("update:modelValue", false);
+};
+</script>
